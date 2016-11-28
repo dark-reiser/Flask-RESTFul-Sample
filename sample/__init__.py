7e32bc635cb6c151395867368d6d20e9c0af1344
Pasithea <Tsukaicyan@gmail.com>
2016-11-28 16:59:40 +0800

    register bp.

--- sample/__init__.py
+++ sample/__init__.py
@@ -4,14 +4,23 @@ from flask.ext.redis import FlaskRedis
 from flask.ext.bcrypt import Bcrypt
 from config import config
 
-redis_store = FlaskRedis()
+rdb = FlaskRedis()
 bcrypt = Bcrypt()
 
 def create_app(config_name):
+    """
+    Entry point to the Flask RESTFul Server application.
+    """
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
-    redis_store.init_app(app)
+    rdb.init_app(app)
     bcrypt.init_app(app)
+
+    from sample.auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    from sample.api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
 
     return app
